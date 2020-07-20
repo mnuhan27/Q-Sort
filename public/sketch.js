9941bdcc243f8727bbe81574c5b09f895df6010f
Nuhan Ahnaf <nuhan.ahanaf@gmail.com>
2020-07-19 19:01:41 -0500

    Repo Initial File Upload

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,19 @@
+let img;
 
-var boxes = 49;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var picNum = getRandomInt(1,66).toString();
+var picName = picNum + '.jpg';
+
+function preload(){
+    img = loadImage('assets/' + picName);
+}
+
+var boxes = 64;
 
 var rowBoxes = [1, 3, 5, 7, 9, 11, 13, 15, 17];
 var rowTot = [1, 4, 9, 16, 25, 36, 49, 64, 81];
@@ -78,6 +92,7 @@ function drawQSort(totalBoxes) {
     console.log("the number of boxes in second drawing: " + rowBoxes[remIndex]);
     console.log(remEven);
 
+    //method for square number of boxes (2,49,36)
     for (let i = 0; i < rowIndex; i++) {
         for (let h = 0; h < rowPositions[i].length; h++) {
             rect(windowWidth / 2 + 100 * rowPositions[rowIndex][h] - 100*i + 500, 100 + 100*i, 100, 100);
@@ -91,19 +106,36 @@ function drawQSort(totalBoxes) {
 
 }
 
-
+let circleX = 100;
+let circleY = 100;
+let diameter = 80;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
 
+
 }
+
+function mouseReleased() {
+    console.log(mouseX, mouseY);
+};
 
 function draw(){
     background(255);
     
     //rectMode(CENTER);
     //rect(windowWidth/2,windowHeight/2,100,100);
+    fill(255);
     drawQSort(boxes);
+
+    if (dist(circleX, circleY, mouseX, mouseY) < diameter / 2 && mouseIsPressed) {
+
+        circleX = mouseX;
+        circleY = mouseY;
+    }
+
+    fill(125);
+    image(img,circleX,circleY,80,80);
     
     
 }
